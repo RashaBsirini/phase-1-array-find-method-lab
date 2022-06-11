7807cbd4104ba2e8e5f5409fa725bc0df5262c1d
RashaBsirini <rashaabduallah1@gmail.com>
2022-06-11 15:43:50 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+
+function superbowlWin(arr)
+{
+    let result=undefined;
+   const res=arr.find(function(element)
+    {
+        if( element.result=="W")
+           {result=element.year;
+             return result}
+    })
+  return result;
+}
